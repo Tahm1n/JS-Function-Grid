905bb94e244c8a3934509f243bfb7bce1c08dba5
Tahmin Ahmed <theosphice97@gmail.com>
2017-02-15 21:09:23 +0000

    Smiley Face

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //Write your function calls below this line
+setColor(3,3,"black");
+setColor(3,6,"black");
+setColor(5,2,"pink");
+setColor(6,3,"pink");
+setColor(6,4,"pink");
+setColor(6,5,"pink");
+setColor(6,6,"pink");
+setColor(5,7,"pink");
+setColor(0,0,"yellow");
